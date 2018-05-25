8ee3093af1c3e636616171bf4f24825b9de6e161
Draz96 <dario.zaccherini@gmail.com>
2018-05-25 11:23:08 +0200

    Primo commit

---
+++ unicorno6/Assets/Scripts/PrintText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // mie funzioni
+    public void salutami (string nome)
+    {
+        print("ciao " + nome);
+    }
+
+}
